89ba1226bbbd42a5b5c504fc7a27d4cda6e35191
Ed Whitwell <edwhitwell1507@gmail.com>
2025-02-28 21:17:28 +0000

    added a placeholder stream

--- src/components/LiveStream.tsx
+++ src/components/LiveStream.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
+
+
 const LiveStream = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const src = "https://ireplay.tv/test/blender.m3u8";
+    useEffect(() => {
+        const video = videoRef.current;
+    
+        if (!video) return;
+    
+        if (Hls.isSupported()) {
+          const hls = new Hls();
+          hls.loadSource(src);
+          hls.attachMedia(video);
+          hls.on(Hls.Events.MANIFEST_PARSED, () => {
+            video.play().catch(err => console.error("Autoplay failed:", err));
+          });
+    
+          return () => {
+            hls.destroy();
+          };
+        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+          video.src = src;
+          video.addEventListener("loadedmetadata", () => {
+            video.play().catch(err => console.error("Autoplay failed:", err));
+          });
+        }
+      }, [src]);
+    
     return (
         <div className="live-stream">
-            <video controls autoPlay width="100%" height="100%">
-                <source src="https://cdn-videos.akamaized.net/va01201/hls/NASA-NTV2-PUB/playlist.m3u8" type="application/x-mpegURL" />
-                Your browser does not support the video tag.
+            <video ref={videoRef} controls autoPlay width="100%" height="100%">
+                
             </video>
         </div>
     );
